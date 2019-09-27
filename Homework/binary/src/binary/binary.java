bd3674894a874e57e707e5d7b05ee1953fcf09cc
Remigijus Romas <remigijus.romas@gmail.com>
2019-09-27 08:55:31 +0300

    homework

---
+++ Homework/binary/src/binary/binary.java
@@ -0,0 +1,15 @@
+package binary;
+
+public class binary {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("Suma: a + b = " + (a + b)+ " binarinė israiska:" + Integer.toBinaryString(a+b));
+		System.out.println("Skirtumas: a - b = " + (a - b)+ " binarinė israiska:" + Integer.toBinaryString(a-b));
+		System.out.println("Daugyba: a * b = " + (a * b)+ " binarinė israiska:" + Integer.toBinaryString(a*b));
+		System.out.println("Dalyba: b / a = " + (b / a)+" binarinė israiska:" + Integer.toBinaryString(b/a));
+
+	}
+
+}
